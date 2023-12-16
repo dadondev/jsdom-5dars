2c504908b9baa03d02216c8d3b0ca7766042300a
dadondev <dadondev03@gmail.com>
2023-12-16 19:17:32 +0500

    jsdom-5-uy-ishi

---
+++ app.js
@@ -0,0 +1,18 @@
+let allCards = document.querySelectorAll(".card");
+
+// allCards.forEach((card) => {
+//   allCards.forEach((event) => {
+//     event.addEventListener("click", () => {
+//       if (event == card) {
+//         if (event.nextElementSibling) {
+//           event.nextElementSibling.classList.remove("hidden");
+//           event.nextElementSibling.classList.add("block", "transition-all");
+//           console.log(event.nextElementSibling);
+//         }
+//       } else {
+//         event.nextElementSibling.classList.remove("block");
+//         event.nextElementSibling.classList.add("hidden");
+//       }
+//     });
+//   });
+// });
